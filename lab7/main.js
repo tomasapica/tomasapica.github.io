6a725f41958a12d25504d4df18d8cbe3dde9f2f0
tomaspica <167481630+tomasapica@users.noreply.github.com>
2024-11-05 20:50:30 +0000

    Added text hover,  color buttons and counter button events to main.js

--- lab7/main.js
+++ lab7/main.js
@@ -1,3 +1,44 @@
-document.getElementById("button").addEventListener("click", function() {
-    alert("Button clicked!");
+document.addEventListener('DOMContentLoaded', function() {
+    // frase muda com mouse
+    const frase = document.querySelector('#passar');
+
+    frase.onmouseover = function() {
+        frase.innerHTML = 'Obrigado por passares!';
+    };
+
+    frase.onmouseout = function() {
+        frase.innerHTML = 'Passa por aqui!';
+    };
+
+    // muda cor frase butoes
+    const cor_texto = document.querySelector('#pintar');
+    const butao_red = document.querySelector('#red'); // botao red
+    const butao_green = document.querySelector('#green'); // botao green
+    const butao_blue = document.querySelector('#blue'); // botao blue
+
+    function mudar_cor(color) {
+        cor_texto.style.color = color;
+    }
+
+    butao_red.onclick = function() {
+        mudar_cor('red');
+    };
+
+    butao_green.onclick = function() {
+        mudar_cor('green');
+    };
+
+    butao_blue.onclick = function() {
+        mudar_cor('blue');
+    };
+
+    // contador butao
+    let counter = 0;
+
+    function count() {
+        counter++;
+        document.querySelector('#contador').innerHTML = counter;
+    }
+
+    document.querySelector('#butao_contar').onclick = count;
 });
