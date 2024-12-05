c0c35473f11ee9fcabe4d6390abf159bc24e40a4
tomaspica <167481630+tomasapica@users.noreply.github.com>
2024-12-05 13:10:12 +0000

    Defesa 2 - commit 1

--- defesa2/javascript/main.js
+++ defesa2/javascript/main.js
@@ -46,6 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
             filtrarProdutosPorCategoria(categoriaSelecionada, produtos);
         });
 
+        // Passo 1 - defesa 2
         // Filtro 2 - Ordenar
         const selectOrdenar = document.createElement('select');
         const labelOrdenar = document.createElement('label');
@@ -57,17 +58,17 @@ document.addEventListener('DOMContentLoaded', function() {
         sectionFiltros.append(labelOrdenar);
         labelOrdenar.append(selectOrdenar);
         selectOrdenar.append(optionOrdenarDefault);
-        optionOrdenarDefault.innerHTML = `Ordenar pelo preço`;
+        optionOrdenarDefault.innerHTML = `Ordenar por qntd. ratings`;
         optionOrdenarDefault.value = "";
         
         selectOrdenar.append(optionOrdenar1);
-        optionOrdenar1.innerHTML = `Preço Decrescente`;
+        optionOrdenar1.innerHTML = `Rating Decrescente`;
         selectOrdenar.append(optionOrdenar2);
-        optionOrdenar2.innerHTML = `Preço Crescente`;
+        optionOrdenar2.innerHTML = `Rating Crescente`;
 
         selectOrdenar.addEventListener('change', () => {
             const ordenacaoSelecionada = selectOrdenar.value;
-            ordenarProdutosPorPreco(ordenacaoSelecionada, produtos);
+            ordenarProdutosPorRatingCount(ordenacaoSelecionada, produtos);
         });
 
         // Filtro 3 - Procurar
@@ -98,11 +99,13 @@ function realizarCompra() {
 
     const checkboxEstudante = document.querySelector('#checkbox-estudante');
     const inputCupao = document.querySelector('#desconto');
+    const nomePessoa = document.querySelector('#nome_pessoa');
 
     const dadosCompra = {
         products: produtosSelecionados.map(produto => produto.id),
         student: checkboxEstudante.checked,
         coupon: inputCupao.value,
+        name: nomePessoa.value, // Passo 5
     };
 
     // buy - evnviar dados via POST
@@ -145,6 +148,33 @@ function resumoCompra(totalCost, reference) {
     referenciaCheckout.innerHTML = `Referência de pagamento: ${reference}`;
 }
 
+// Passo 3 - defesa 2
+function carregarProdutosSemDescricao(produtos) {
+    const sectionArtigos = document.querySelector('#artigos');
+    sectionArtigos.innerHTML = "";
+    produtos.forEach(produto => {
+        console.log(`id: ${produto.id}, título: ${produto.title}`);
+        const artigoSemDescricao = criaProdutoCesto(produto);
+        sectionArtigos.append(artigoSemDescricao);
+    });
+}
+
+// butao menos info
+document.querySelector('#menos_info').addEventListener('click', carregarProdutosSemDescricao);
+
+// Passo 2 - defesa 2
+// funcao remover tudo
+function removerProdutosCesto() {
+    let cesto = document.querySelector('#artigos-cesto');
+    cesto.innerHTML = "";
+    const lista = [];
+    localStorage.setItem('produtos-selecionados', JSON.stringify(lista));
+    atualizarPrecoTotal(lista);
+}
+
+// butao remover tudo do cesto
+document.querySelector('#remover_produtos_cesto').addEventListener('click', removerProdutosCesto);
+
 // butao comprar
 document.querySelector('#comprar').addEventListener('click', realizarCompra);
 
@@ -158,13 +188,13 @@ function procurarProdutosPorTitulo(texto, produtos) {
     carregarProdutos(produtosFiltrados);
 }
 
-function ordenarProdutosPorPreco(ordenacao, produtos) {
+function ordenarProdutosPorRatingCount(ordenacao, produtos) {
     let produtosOrdenados;
 
-    if (ordenacao === "Preço Crescente") {
-        produtosOrdenados = produtos.sort((a, b) => a.price - b.price);
-    } else if (ordenacao === "Preço Decrescente") {
-        produtosOrdenados = produtos.sort((a, b) => b.price - a.price);
+    if (ordenacao === "Rating Crescente") {
+        produtosOrdenados = produtos.sort((a, b) => a.rating.count - b.rating.count);
+    } else if (ordenacao === "Rating Decrescente") {
+        produtosOrdenados = produtos.sort((a, b) => b.rating.count - a.rating.count);
     } else {
         produtosOrdenados = produtos;
     }
